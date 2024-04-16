c9fcd16d52bf0c8f19a64401c35fa8fff67c1d98
Johan <johan.svensson@gtsmarketing.se>
2024-04-16 09:02:23 +0200

    added add-name

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ let counter = document.getElementById("counter");
 let dec = document.getElementById("dec");
 inc.onclick = incClick;
 dec.onclick = decClick;
-let names = document.getElementById("names");
 names.onmouseover = hover;
 names.onmouseout = hoverLeave;
+
+let addName = document.getElementById("addName");
+let nameInput = document.querySelector("#names input[type='text']");
+let nameList = document.querySelector("#names ul");
+
+addName.onclick = addNameToList;
+
+
 
 function incClick() {
     let number = 0;
@@ -22,7 +29,7 @@ function decClick() {
 
 function hover(e) {
     let name = e.target;
-    if (e.target.tagName === 'LI') {
+    if (e.target.tagName === 'li') {
         e.target.style.backgroundColor = "grey";
     }
 
@@ -33,4 +40,17 @@ function hoverLeave(e) {
     name.style.backgroundColor = "";
 
 }
+
+function addNameToList(){
+    let newname = nameInput.value;
+    if (newname) {
+        let newLi = document.createElement("li");
+        newLi.textContent = newname;
+        nameList.appendChild(newLi)
+        nameInput.value = "";
+
+    }
+}
+    
+
 
